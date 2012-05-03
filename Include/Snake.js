5a67bf94c5295cb0122188adcb96af06cf2a2a29
Alexander Pinnecke <alexander.pinnecke@googlemail.com>
2012-05-04 00:24:06 +0200

    - added reset Button to index.html - added Informations about Levels etc. to index.html - continued developing the Game in Snake.js - added Level System
    - first running version

--- Include/Snake.js
+++ Include/Snake.js
@@ -1,104 +1,157 @@
 $ ( document ).ready ( function ( ) {
-	var snake = new Snake (
+	var s = new Snake (
 		{
 		canvas: $ ( "#CanvasSnake" ) ,
 		startPosx: 25 ,
 		startPosy: 25 ,
 		startDirection: "right" ,
 		speed: 15 ,
-		grid: 5 ,
+		grid: 10 ,
 		startFoodCount: 1 ,
-		levelSteps: 15 ,
+		levelStep: 1.5 ,
 		startLength: 5 ,
-		colorSnake: "#ff0000" ,
-		colorFood: "#00ff00"
+		colorSnake: "#000000" ,
+		colorFood: "#00ff00" ,
+		colorSuperFood: "#FF0000" ,
+		weightSuperFood: 3 ,
+		weightFood: 1 ,
+		elements:
+			{
+			"startButton": $ ( "#start" ) ,
+			"pauseButton": $ ( "#pause" ) ,
+			"resetButton": $ ( "#reset" ) ,
+			"eatenFood": $ ( "#eatenFood" ) ,
+			"levelOutput": $ ( "#level" ) ,
+			"nextLevelOutput": $ ( "#nextLevel" )
+			}
 		} );
-	snake.start ( );
+	s.init ( );
+	s.start ( );
 } );
 
 var Snake = function ( settings ) {
 	
+	this.settings = settings;
+	
 	this.graphic = settings.canvas.get ( 0 ).getContext ( "2d" );
-	this.moveDirection = settings.startDirection;
+	this.weightSuperFood = settings.weightSuperFood;
 	this.maxHeight = settings.canvas.height ( );
 	this.maxWidth = settings.canvas.width ( );
-	this.foodCount = settings.startFoodCount;
-	this.levelsteps = settings.levelSteps;
 	this.colorSnake = settings.colorSnake;
+	this.weightFood = settings.weightFood;
 	this.colorFood = settings.colorFood;
-	this.length = settings.startLength;
-	this.pos = new Dot ( settings.startPosx, settings.startPosy, false );
+	this.levelStep = settings.levelStep;
+	this.elements = settings.elements;
 	this.speed = settings.speed;
 	this.grid = settings.grid;
-	
-	this.tail = new Array ( );
-	this.food = new Array ( );
+	this.moveDirection = null;
 	this.intervalObj = null;
-	this.keyPressed = false;
-	this.pause = true;
-	this.level = 1;
-	
-	this.start = function ( ) {
-		thatSnake = this;
-		
-		$ ( document ).on (
-			{
-			"keydown": function ( event ) {
-				if ( thatSnake.keyPressed || thatSnake.pause )
-					return;
-				
-				thatSnake.keyPressed = true;
-				// left: 37, up: 38, right: 39, down 40
-				switch ( event.which ) {
-					case 37 :
-						if ( thatSnake.moveDirection != "left" && thatSnake.moveDirection != "right" )
-							thatSnake.moveDirection = "left";
-						break;
-					case 38 :
-						if ( thatSnake.moveDirection != "up" && thatSnake.moveDirection != "down" )
-							thatSnake.moveDirection = "up";
-						break;
-					case 39 :
-						if ( thatSnake.moveDirection != "left" && thatSnake.moveDirection != "right" )
-							thatSnake.moveDirection = "right";
-						break;
-					case 40 :
-						if ( thatSnake.moveDirection != "up" && thatSnake.moveDirection != "down" )
-							thatSnake.moveDirection = "down";
-						break;
-					default :
-
-						break;
-				}
-			} ,
-			"keyup": function ( ) {
-				if ( thatSnake.pause )
-					return;
-				thatSnake.keyPressed = false;
-			}
-			} );
-		
-		$ ( "#start" ).on ( "click" , function ( ) {
-			if ( thatSnake.intervalObj == null )
-				thatSnake.intervalObj = window.setInterval ( "thatSnake.move();" , 1000 / thatSnake.speed );
-		} );
-		
-		$ ( "#stop_pause" ).on ( "click" , function ( ) {
-			if ( thatSnake.intervalObj != null )
-				window.clearInterval ( thatSnake.intervalObj );
-		} );
-		
-		this.init ( );
-		this.draw ( );
-	};
+	this.foodCount = null;
+	this.nextLevel = 0;
+	this.length = null;
+	this.failed = null;
+	this.pause = null;
+	this.level = null;
+	this.tail = null;
+	this.food = null;
+	this.pos = null;
 	
 	this.init = function ( ) {
-		var dot = new Dot ( this.posx, this.posy );
+		thatSnake = this;
 		
-		for ( var i = 1 ; i <= this.length ; i ++ ) {
-			this.tail.push ( this.cloneDot ( dot ) );
-			dot = this.moveDot ( dot );
+		settings.startPosx -= ( ( settings.startPosx % this.grid ) - 1 );
+		if ( settings.startPosx < this.grid )
+			settings.startPosx = this.grid * 2;
+		if ( settings.startPosx > this.maxWidth - this.grid )
+			settings.startPosx = this.maxWidth - this.grid * 2;
+		
+		settings.startPosy -= ( ( settings.startPosy % this.grid ) - 1 );
+		if ( settings.startPosy < this.grid )
+			settings.startPosy = this.grid * 2;
+		if ( settings.startPosy > this.maxHeight - this.grid )
+			settings.startPosy = this.maxHeight - this.grid * 2;
+		
+		$ ( document ).on ( "keydown" , function ( event ) {
+			if ( thatSnake.pause )
+				return;
+			
+			// left: 37, up: 38, right: 39, down 40
+			switch ( event.which ) {
+				case 37 :
+					if ( thatSnake.moveDirection != "left" && thatSnake.moveDirection != "right" )
+						thatSnake.moveDirection = "left";
+					break;
+				case 38 :
+					if ( thatSnake.moveDirection != "up" && thatSnake.moveDirection != "down" )
+						thatSnake.moveDirection = "up";
+					break;
+				case 39 :
+					if ( thatSnake.moveDirection != "left" && thatSnake.moveDirection != "right" )
+						thatSnake.moveDirection = "right";
+					break;
+				case 40 :
+					if ( thatSnake.moveDirection != "up" && thatSnake.moveDirection != "down" )
+						thatSnake.moveDirection = "down";
+					break;
+				default :
+
+					break;
+			}
+		} );
+		
+		this.elements.startButton.on ( "click" , function ( ) {
+			if ( thatSnake.intervalObj == null ) {
+				thatSnake.pause = false;
+				thatSnake.intervalObj = window.setInterval ( "thatSnake.move();" , 1000 / thatSnake.speed );
+			}
+		} );
+		
+		this.elements.resetButton.on ( "click" , function ( ) {
+			if ( thatSnake.intervalObj == null && thatSnake.failed )
+				thatSnake.start ( );
+		} );
+		
+		this.elements.pauseButton.on ( "click" , function ( ) {
+			if ( thatSnake.intervalObj != null ) {
+				window.clearInterval ( thatSnake.intervalObj );
+				thatSnake.intervalObj = null;
+			}
+			
+		} );
+	};
+	
+	this.start = function ( ) {
+		
+		this.pos = new Dot ( this.settings.startPosx, this.settings.startPosy, false );
+		this.moveDirection = this.settings.startDirection;
+		this.foodCount = this.settings.startFoodCount;
+		this.length = this.settings.startLength;
+		this.tail = new Array ( );
+		this.food = new Array ( );
+		this.intervalObj = null;
+		this.failed = false;
+		this.pause = true;
+		this.level = 0;
+		
+		this.graphic.clearRect ( 0 , 0 , this.maxWidth , this.maxHeight );
+		
+		// Fill output fields with first values
+		this.output ( );
+		
+		// Add |length| dots to tail
+		this.tail.push ( this.cloneDot ( this.pos ) );
+		for ( var i = 1 ; i < this.length ; i ++ ) {
+			this.pos = this.moveDot ( this.pos );
+			this.tail.push ( this.cloneDot ( this.pos ) );
 		}
+		
+		// add new Foods
+		this.addFoods ( );
+		
+		// Draw Snake
+		this.draw ( );
+		
+		this.setLevel ( );
 	};
 	
 	this.draw = function ( ) {
@@ -111,10 +164,51 @@ var Snake = function ( settings ) {
 		}
 	};
 	
+	this.move = function ( ) {
+		this.pos = this.moveDot ( this.pos );
+		
+		if ( this.isFoodDot ( this.pos ) ) {
+			this.removeFood ( this.pos );
+			this.removeDot ( this.pos );
+			this.length ++ ;
+			this.setLevel ( );
+			this.addFoods ( );
+		}
+		else if ( this.isTailDot ( this.pos ) ) {
+			this.failed = true;
+			this.elements.pauseButton.trigger ( "click" );
+			alert ( "Failed at Tail" );
+			return;
+		}
+		else if ( this.isBorderDot ( this.pos ) ) {
+			this.failed = true;
+			this.elements.pauseButton.trigger ( "click" );
+			alert ( "Failed at Border" );
+			return;
+		}
+		else {
+			var lastDot = this.tail.shift ( );
+			this.removeDot ( lastDot );
+		}
+		
+		this.tail.push ( this.cloneDot ( this.pos ) );
+		this.drawDot ( this.pos );
+	};
+	
+	// DOT OPERATIONS
+	
 	this.drawDot = function ( dot , style ) {
 		this.graphic.fillStyle = style;
 		this.graphic.strokeStyle = style;
 		this.graphic.fillRect ( dot.getx ( ) , dot.gety ( ) , this.grid , this.grid );
+	};
+	
+	this.cloneDot = function ( dot ) {
+		return new Dot ( dot.getx ( ), dot.gety ( ), dot.getDrawState ( ) );
+	};
+	
+	this.removeDot = function ( dot ) {
+		this.graphic.clearRect ( dot.getx ( ) , dot.gety ( ) , this.grid , this.grid );
 	};
 	
 	this.moveDot = function ( dot ) {
@@ -130,19 +224,87 @@ var Snake = function ( settings ) {
 		return dot;
 	};
 	
-	this.isValidDot = function ( dot ) {
-		for ( var index in this.tail.concat ( this.food ) )
+	this.isValidDot = function ( dot , secure ) {
+		return ( ! ( this.isTailDot ( dot ) || this.isFoodDot ( dot ) || ( ( typeof secure != "undefined" && secure == true ) ? this.isNearBorderDot ( dot ) : false ) ) );
+	};
+	
+	this.isTailDot = function ( dot ) {
+		for ( var index in this.tail )
 			if ( dot.getx ( ) == this.tail [ index ].getx ( ) && dot.gety ( ) == this.tail [ index ].gety ( ) )
-				return false;
-		
+				return true;
+		return false;
 	};
 	
-	this.cloneDot = function ( dot ) {
-		return new Dot ( dot.getx ( ), dot.gety ( ), dot.getDrawState ( ) );
+	this.isFoodDot = function ( dot ) {
+		for ( var index in this.food )
+			if ( dot.getx ( ) == this.food [ index ].getPos ( ).getx ( ) && dot.gety ( ) == this.food [ index ].getPos ( ).gety ( ) )
+				return true;
+		return false;
 	};
 	
-	this.move = function ( ) {
+	this.isBorderDot = function ( dot ) {
+		return ( dot.getx ( ) <= 0 || dot.getx ( ) >= this.maxWidth || dot.gety ( ) <= 0 || dot.gety ( ) >= this.maxHeight );
+	};
+	
+	this.isNearBorderDot = function ( dot ) {
+		return ( dot.getx ( ) < this.grid || ( dot.getx ( ) >= ( this.maxWidth - this.grid ) ) || dot.gety ( ) < this.grid || ( dot.gety ( ) >= ( this.maxHeight - this.grid ) ) );
+	};
+	
+	// FOOD OPERATIONS
+	
+	this.newFood = function ( weight ) {
+		var dot = new Dot ( Math.round ( Math.random ( ) * this.maxWidth ), Math.round ( Math.random ( ) * this.maxHeight , false ) );
+		dot.setx ( dot.getx ( ) - ( ( dot.getx ( ) % this.grid ) - 1 ) );
+		dot.sety ( dot.gety ( ) - ( ( dot.gety ( ) % this.grid ) - 1 ) );
 		
+		return ( this.isValidDot ( dot , true ) ) ? new Food ( dot, weight ) : this.newFood ( );
+	};
+	
+	this.addFoods = function ( ) {
+		while ( this.food.length < this.foodCount ) {
+			var rand = Math.random ( );
+			
+			if ( rand < 0.15 ) { // SuperFood
+				var food = this.newFood ( this.weightSuperFood );
+				this.food.push ( food );
+				this.drawDot ( food.getPos ( ) , this.colorSuperFood );
+			}
+			else {
+				var food = this.newFood ( this.weightFood );
+				this.food.push ( food );
+				this.drawDot ( food.getPos ( ) , this.colorFood );
+			}
+			
+		}
+	};
+	
+	this.removeFood = function ( dot ) {
+		if ( dot instanceof Food )
+			dot = dot.getPos ( );
+		
+		for ( var index in this.food )
+			if ( dot.getx ( ) == this.food [ index ].getPos ( ).getx ( ) && dot.gety ( ) == this.food [ index ].getPos ( ).gety ( ) )
+				return this.food.splice ( index , 1 );
+	};
+	
+	// LEVEL OPERATIONS
+	
+	this.setLevel = function ( ) {
+		if ( this.length >= this.nextLevel ) {
+			this.level ++ ;
+			this.nextLevel = this.level + Math.round ( this.length * this.levelStep );
+			this.foodCount = Math.round ( this.level / 2 );
+		}
+		
+		this.output ( );
+	};
+	
+	// OUTPUT OPERATIONS
+	
+	this.output = function ( ) {
+		this.elements.eatenFood.text ( this.length );
+		this.elements.levelOutput.text ( this.level );
+		this.elements.nextLevelOutput.text ( this.nextLevel );
 	};
 };
 
